26486af1f3994bcde2fb32c7944b6d35cf0053e5
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-01-09 23:22:16 +0300

    Add ThemeToIconConverter

---
+++ PasswordKeeper/PasswordKeeper/Converters/ThemeToIconConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Data.Converters;
+using Material.Icons;
+using System;
+using System.Globalization;
+
+namespace PasswordKeeper.Converters;
+public class ThemeToIconConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isDarkTheme)
+            return isDarkTheme ? MaterialIconKind.WeatherNight : MaterialIconKind.WeatherSunny;
+        return MaterialIconKind.WeatherSunny;
+    }
+
+    // We don't need to convert back from icon to theme for this use case
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
