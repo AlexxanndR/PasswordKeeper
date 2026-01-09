5e49d1427c2e109274e91211ff0e0a1b83e86c34
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-01-09 22:50:31 +0300

    Add themes toggle logic

--- PasswordKeeper/PasswordKeeper/ViewModels/MainViewModel.cs
+++ PasswordKeeper/PasswordKeeper/ViewModels/MainViewModel.cs
@@ -1,9 +1,19 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace PasswordKeeper.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    private bool? _isDarkTheme = Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
+
+    [RelayCommand]
+    private void ToggleTheme()
+    {
+        if (Application.Current is { } app)
+            Application.Current.RequestedThemeVariant = IsDarkTheme.HasValue && IsDarkTheme.Value ? ThemeVariant.Dark : ThemeVariant.Light;
+    }
 }
