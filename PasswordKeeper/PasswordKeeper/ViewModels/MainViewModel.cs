88a6e4602f49e72d90aa8a9cc49ac7682aababda
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-01-12 21:25:20 +0300

    Add password manager view opening logic

--- PasswordKeeper/PasswordKeeper/ViewModels/MainViewModel.cs
+++ PasswordKeeper/PasswordKeeper/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Styling;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -10,11 +11,20 @@ public partial class MainViewModel : ViewModelBase
     [ObservableProperty]
     private bool? _isDarkTheme = Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
 
+    [ObservableProperty]
+    private UserControl? _currentView;
+
     [RelayCommand]
     private void ToggleTheme()
     {
         IsDarkTheme = IsDarkTheme.HasValue && !IsDarkTheme.Value;
         if (Application.Current is { } app)
             Application.Current.RequestedThemeVariant = IsDarkTheme.HasValue && IsDarkTheme.Value ? ThemeVariant.Dark : ThemeVariant.Light;
+    }
+
+    [RelayCommand]
+    private void AddPassword()
+    {
+        CurrentView = new PasswordManagerView();
     }
 }
