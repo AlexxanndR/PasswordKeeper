4bfd4ccc3614047e3919819cfa9fe6fa88f6d79f
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-01-23 21:01:01 +0300

    Add first DI usage

--- PasswordKeeper/PasswordKeeper/App.axaml.cs
+++ PasswordKeeper/PasswordKeeper/App.axaml.cs
@@ -1,11 +1,12 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using Mvvm.Navigation;
 using PasswordKeeper.ViewModels;
 using PasswordKeeper.Views;
+using System.Linq;
 
 namespace PasswordKeeper;
 
@@ -18,6 +19,13 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var collection = new ServiceCollection();
+        collection.AddViewModels();
+        collection.AddCommonServices();
+
+        var services = collection.BuildServiceProvider();
+
+        var vm = services.GetRequiredService<MainViewModel>();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
@@ -25,14 +33,14 @@ public partial class App : Application
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = vm
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = vm
             };
         }
 
---
+++ PasswordKeeper/PasswordKeeper/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PasswordKeeper.ViewModels;
+
+namespace Mvvm.Navigation;
+public static partial class ServiceCollectionExtensions
+{
+    public static void AddViewModels(this IServiceCollection collection)
+    {
+        collection.AddSingleton<MainViewModel>();
+        collection.AddTransient<PasswordsListViewModel>();
+        collection.AddTransient<PasswordManagerViewModel>();
+    }
+}
