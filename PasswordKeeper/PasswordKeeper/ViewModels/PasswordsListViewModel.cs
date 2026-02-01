4d79d6eadb1f8bcca2a9ecb8fad66d66d561f9ab
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-02-01 16:02:09 +0300

    Add passwords property to PasswordsListViewModel

--- PasswordKeeper/PasswordKeeper/ViewModels/PasswordsListViewModel.cs
+++ PasswordKeeper/PasswordKeeper/ViewModels/PasswordsListViewModel.cs
@@ -1,9 +1,14 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PasswordKeeper.Navigation;
+using System.Collections.ObjectModel;
 
 namespace PasswordKeeper.ViewModels;
 public partial class PasswordsListViewModel(INavigator navigator) : ViewModelBase
 {
+    [ObservableProperty]
+    private ObservableCollection<PasswordViewModel> _passwords = [];
+
     [RelayCommand]
     private void AddPassword()
         => navigator.NavigateTo<PasswordManagerViewModel>();
