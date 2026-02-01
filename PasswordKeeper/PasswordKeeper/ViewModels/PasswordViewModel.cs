2896daf889a329661d3f4abc31616e9912ce3ce4
AlexxanndR <alexandrov.alexandder@gmail.com>
2026-02-01 16:01:06 +0300

    Add PasswordViewModel

---
+++ PasswordKeeper/PasswordKeeper/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PasswordKeeper.ViewModels;
+public partial class PasswordViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _icon;
+
+    [ObservableProperty]
+    private string _name;
+}
